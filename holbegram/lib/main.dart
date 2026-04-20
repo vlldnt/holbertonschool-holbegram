b80c5be26bfc4b4df18d8f6c2e18f0bddd83b2b3
vlldnt <vieilledent.adrien@gmail.com>
2026-04-20 09:41:20 +0000

    Add Windows runner files for Flutter application
    - Created `runner.exe.manifest` to define application settings and DPI awareness.
    - Implemented `utils.cpp` and `utils.h` for console creation and command line argument handling.
    - Developed `win32_window.cpp` and `win32_window.h` to manage a high DPI-aware Win32 window, including theme updates and message handling.

---
+++ holbegram/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Holbegram',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const Scaffold(
+        body: Center(child: Text('Holbegram')),
+      ),
+    );
+  }
+}
---
+++ holbegram/lib/screens/upload_image_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class UploadImageScreen extends StatefulWidget {
+  const UploadImageScreen({super.key});
+
+  @override
+  State<UploadImageScreen> createState() => _UploadImageScreenState();
+}
+
+class _UploadImageScreenState extends State<UploadImageScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Upload Image Screen'),
+      ),
+    );
+  }
+}
